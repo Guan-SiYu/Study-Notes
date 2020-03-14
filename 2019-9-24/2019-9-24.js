3ecceeb254a278d764df685b36bcfb5788ce86d9
关丝予 <58887477+Guan-SiYu@users.noreply.github.com>
2020-03-14 23:47:02 +0800

    Add files via upload

---
+++ 2019-9-24/2019-9-24.js
@@ -0,0 +1,80 @@
+
+
+var ol = new Vue({
+    el:"#ol",
+    data:{
+        item:[6,7,8],
+        what:'whatthodsahkjfh',
+        group:[
+            {
+              name:'Aa',
+
+              info:{
+                  age:18,
+                  gender:'girl',
+                  ins:this.ins,
+                  what:this.what
+              }
+            },
+            {
+                name:'Aa',
+                info:{
+                    age:18,
+                    gender:'girl',
+                    ins:this.ins,
+                    what:this.what
+
+                }
+            },
+            {
+                name:'Aa',
+                info:{
+                    age:18,
+                    gender:'girl',
+                    ins:this.ins,
+                    what:this.what
+
+                }
+            },
+            {
+                name:'Aa',
+                info:{
+                    age:18,
+                    gender:'girl',
+                    ins:this.ins,
+                    what:this.what
+
+                }
+            },
+            {
+                name:'Aa',
+                info:{
+                    age:18,
+                    gender:'girl',
+                    ins:this.ins,
+                    what:this.what
+
+                }
+            },
+        ]
+    }
+});
+var ul = new Vue({
+    el:"#ul",
+    data:{
+        obj:{
+            one:'how are you today',
+            two:'welcome to GuangZhou',
+            three:'i dont want to go to class',
+            four:'i just like sleep and eat'
+        }
+    }
+});
+
+
+var exm1 = new Vue({
+    el:'#exm1',
+    data:{
+        counter:0
+    }
+});
